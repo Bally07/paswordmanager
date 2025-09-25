4a17ef6c40297f5c64ff652b92f473f38382782a
Bally07 <126963429+Bally07@users.noreply.github.com>
2025-09-25 15:16:04 +0100

    comit

--- winforms/security.cs
+++ winforms/security.cs
@@ -1,7 +1,19 @@
-USING system.security.cryptography
-using Random
-puplic class security
+using system.security.cryptography;
+using Random;
+using System.Text;
+using System;
+
+public class security
 {
+    public static void Main()
+    { 
+    string key = "mysecretkey";
+    string input = "mydata";
+    input == salt(input);
+    key == hasher(key);
+    Console.Writeline(encrypt(input,key)); 
+    }
+    
     public string hasher(string input)
     {
         using (SHA256 sha256Hash = SHA256.Create())
@@ -16,7 +28,7 @@ puplic class security
         string saltedInput;
         var random = new Random();
         int saltLength = 2;
-        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\x02";
         for (int i = 0; i < saltLength; i++)
         {
             Salt=+ chars(random.next());
@@ -24,6 +36,12 @@ puplic class security
           saltedInput = Salt + ":" + input;
           return saltedInput;
     } 
+    public string encrypt (string input ,string key) 
+    {
+        string encryptedString = "";
+        encryptedstring = input ^ key;
+        return encryptedString;
+    }
 }
 
 
